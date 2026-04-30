6c0f50270ee7ffea4599233d046dd2a5e1821433
your-username <your-email@example.com>
2026-04-29 19:37:39 -0700

    Update scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -19,13 +19,13 @@ if [ -z "$1" ] || [ -z "$2" ]; then
     echo "Usage: $0 <repo_id> <policy_repo_id> <batch_size> <steps>"
     exit 1
 fi
-lerobot-train \  
-  --dataset.repo_id="$1" \  
-  --policy.type=act \  
-  --output_dir="~/franka_data/policy/train/act_$1" \  
-  --job_name="act_$1" \  
-  --policy.device=cuda \  
-  --wandb.enable=true \  
+lerobot-train \
+  --dataset.repo_id="$1" \
+  --policy.type=act \
+  --output_dir="~/franka_data/policy/train/act_$1" \
+  --job_name="act_$1" \
+  --policy.device=cuda \
+  --wandb.enable=true \
   --policy.repo_id="$2" \
   --batch_size="$3" \
   --steps="$4"
